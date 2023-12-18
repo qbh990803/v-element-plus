bcbb031c4a88e9b02cc549ba33e7a782bb127b9c
邱彬华 <15979005807@163.com>
2023-12-18 22:01:20 +0800

    feat: 项目初始化

---
+++ env.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="vite/client" />
+
+// declare module '@vue/runtime-core' {
+//   export interface App {
+//     [INSTALLED_KEY]?: boolean
+//   }
+
+//   export interface GlobalComponents {
+//     Component: (props: { is: Component | string }) => void
+//   }
+
+//   export interface ComponentCustomProperties {
+//     vShow: typeof vShow
+//   }
+// }
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,42 @@
+<template>
+  <button
+    ref="_ref"
+    class="vk-button"
+    :class="{
+      [`vk-button--${type}`]: type,
+      [`vk-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+    }"
+    :disabled="disabled"
+    :autofocus="autofocus"
+    :type="nativeType"
+  >
+    <span>
+      <slot></slot>
+    </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import type { ButtonProps } from "./types";
+
+defineOptions({
+  name: 'VKButton'
+});
+
+withDefaults(defineProps<ButtonProps>(), {
+  nativeType: 'button',
+});
+
+const _ref = ref<HTMLButtonElement>()
+
+defineExpose({
+  ref: _ref
+})
+</script>
+
+<style scoped></style>
