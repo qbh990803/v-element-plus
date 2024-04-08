d2260efda0b61d86735062d099ec75ae53fa3280
邱彬华 <15979005807@163.com>
2024-04-08 19:26:24 +0800

    feat: 使用postcss生成对应的css颜色变量

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,12 @@
 /* eslint-env node */
 module.exports = {
   plugins: [
-    require('postcss-nested'),
-    require('postcss-each'),
-  ]
-}
+    require("postcss-each-variables"),
+    require("postcss-nested"),
+    require("postcss-each")({
+      plugins: {
+        beforeEach: [require("postcss-for"), require("postcss-color-mix")],
+      },
+    }),
+  ],
+};
