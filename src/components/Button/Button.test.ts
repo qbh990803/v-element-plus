273167e33a93575aac8963aff94b9fb3910d93ce
邱彬华 <15979005807@163.com>
2024-07-14 12:11:00 +0800

    feat: 给Button添加测试用例

--- src/components/Button/Button.test.ts
+++ src/components/Button/Button.test.ts
@@ -1,6 +1,8 @@
 import { describe, test, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 import Button from "./Button.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import Icon from "../Icon/Icon.vue"; 
 
 describe("Button.vue", () => {
   test("basic button", () => {
@@ -12,7 +14,64 @@ describe("Button.vue", () => {
         default: "button",
       },
     });
+    // class
+    expect(wrapper.classes()).toContain('vk-button--primary')
+    // slot
+    expect(wrapper.get('button').text()).toBe('button')
+    // events
+    wrapper.get('button').trigger('click')
+    expect(wrapper.emitted()).toHaveProperty('click')
+  })
+  test('disabled button', () => {
+    const wrapper = mount(Button, {
+      props: {
+        disabled: true
+      },
+      slots: {
+        default: "button",
+      },
+    });
+    // attributes
+    expect(wrapper.attributes('disabled')).toBeDefined()
+    // attributes 
+    expect(wrapper.find('button').attributes('disabled')).toBeDefined()
+    // events
+    wrapper.get('button').trigger('click')
+    expect(wrapper.emitted()).not.toHaveProperty('click')
+  })
+  test('icon button', () => {
+    const wrapper = mount(Button, {
+      props: {
+        icon:'arrow-up'
+      },
+      slots: {
+        default: "icon",
+      },
+      global: {
+        stubs: ['FontAwesomeIcon']
+      }
+    });
     console.log(wrapper.html())
-    expect(wrapper.classes()).includes('vk-button-primary')
-  });   
+    const iconEl = wrapper.findComponent(FontAwesomeIcon)
+    expect(iconEl.exists()).toBe(true)
+    expect(iconEl.attributes('icon')).toBe('arrow-up')
+  })
+  test('loading button', () => {
+    const wrapper = mount(Button, {
+      props: {
+        loading: true
+      },
+      slots: {
+        default: "loading",
+      },
+      global: {
+        stubs: ['Icon']
+      }
+    });
+    console.log(wrapper.html())
+    const iconEl = wrapper.findComponent(Icon)
+    expect(iconEl.exists()).toBe(true)
+    expect(iconEl.attributes('icon')).toBe('spinner')
+    expect(wrapper.attributes('disabled')).toBeDefined()
+  })
 });
